835a365d2512b4b43cc417f1d8226976f21a9979
Dung246 <dungoo2605@gmail.com>
2026-03-31 23:30:12 +0700

    Update AuthService.java

--- src/business/AuthService.java
+++ src/business/AuthService.java
@@ -1,4 +1,64 @@
 package business;
 
+import entity.User;
+import utils.DBConnection;
+
+import java.sql.*;
+
 public class AuthService {
+
+    public boolean register(User u) {
+        String sql = """
+INSERT INTO users(username, password, full_name, email, phone, department)
+VALUES (?, ?, ?, ?, ?, ?)
+""";
+
+        try (Connection conn = DBConnection.getConnection()) {
+
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, u.getUsername());
+            ps.setString(2, u.getPassword());
+            ps.setString(3, u.getFullName());
+            ps.setString(4, u.getEmail());
+            ps.setString(5, u.getPhone());
+            ps.setString(6, u.getDepartment());
+
+            int rows =ps.executeUpdate();
+            return rows >0;
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // 🔥 in lỗi thật
+            System.out.println("Lỗi đăng ký (có thể trùng username/email)");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
+    public User login(String username, String password) {
+        String sql = "SELECT * FROM users WHERE username=? AND password=?";
+
+        try (Connection conn = DBConnection.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                User u = new User();
+                u.setId(rs.getInt("id"));
+                u.setUsername(rs.getString("username"));
+                u.setFullName(rs.getString("full_name")); //  QUAN TRỌNG
+                u.setRole(rs.getString("role"));
+
+                return u;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
