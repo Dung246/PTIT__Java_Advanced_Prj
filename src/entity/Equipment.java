e7c487361460e3775841eef0505fa9e405b7aefc
Dung246 <dungoo2605@gmail.com>
2026-03-31 23:34:37 +0700

    Enhance Equipment class with properties and constructors
    Added fields and constructors to the Equipment class.

--- src/entity/Equipment.java
+++ src/entity/Equipment.java
@@ -1,4 +1,32 @@
 package entity;
 
 public class Equipment {
+
+    private int id;
+    private String name;
+    private int availableQuantity;
+
+    public Equipment() {}
+
+    public Equipment(String name, int availableQuantity) {
+        this.name = name;
+        this.availableQuantity = availableQuantity;
+    }
+
+    public Equipment(int id, String name, int availableQuantity) {
+        this.id = id;
+        this.name = name;
+        this.availableQuantity = availableQuantity;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAvailableQuantity() { return availableQuantity; }
+    public void setAvailableQuantity(int availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
 }
