ba75561d39b0ab8f2de20f81b93ad16bc7127927
Dung246 <dungoo2605@gmail.com>
2026-03-31 23:36:31 +0700

    Update PasswordUtil.java

--- src/utils/PasswordUtil.java
+++ src/utils/PasswordUtil.java
@@ -1,4 +1,23 @@
 package utils;
 
+import java.security.MessageDigest;
+
 public class PasswordUtil {
+
+    public static String hash(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hashBytes = md.digest(password.getBytes());
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashBytes) {
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
